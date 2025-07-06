adaee3c504a50322b9ff774e5b40a80b2877d919
sln565 <sln5658@gmail.com>
2025-07-06 16:01:04 -0500

    Test Case

---
+++ test_app.py
@@ -0,0 +1,11 @@
+# test_app.test_app
+
+import unittest
+from app import hello-world
+
+class Test_App(unittest.TestCase):
+    def test_hello_world(self)
+        self.assertEqual(hello_world(), "Hello, World")
+
+    if __name__ == '__main__':
+        unittest.main()
